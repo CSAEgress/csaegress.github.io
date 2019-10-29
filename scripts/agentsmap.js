57ee09d672dc52740750b1cd5b0ee733b285d6c4
Chaobai Li <contact@chaobai.li>
2019-10-29 11:47:52 +0100

    zoom in and display agent id on map

--- scripts/agentsmap.js
+++ scripts/agentsmap.js
@@ -20,6 +20,7 @@ const AGENT_COORDINATES = (function(){
 const DEFAULT_CENTER = (function(){
     var lat=0, lng=0, n=0;
     for(var agent_id in AGENT_COORDINATES){
+        if(Math.abs(AGENT_COORDINATES[agent_id][1]) > 30) continue;
         lat += AGENT_COORDINATES[agent_id][0];
         lng += AGENT_COORDINATES[agent_id][1];
         n += 1;
@@ -39,24 +40,46 @@ const MARKERS = {
 };
 
 
-var map = L.map("map").setView(DEFAULT_CENTER, 5);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+var map = L.map("map").setView(DEFAULT_CENTER, 4);
 
+
+var cartoAttr = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
+var cartoUrl = 'http://{s}.basemaps.cartocdn.com/{theme}/{z}/{x}/{y}.png';
+L.tileLayer(cartoUrl,{attribution:cartoAttr,theme:'dark_all'}).addTo(map);
+
+/*L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);*/
+
+
+var agentNameLabels = [];
 
 for(var agent_id in AGENTS){
     var agent = AGENTS[agent_id];
     if(AGENT_COORDINATES[agent_id]){
-        L.marker(AGENT_COORDINATES[agent_id], {
+        var marker = L.marker(AGENT_COORDINATES[agent_id], {
             icon: MARKERS[agent.faction],
             title: agent_id,
             alt: agent_id,
-        }).addTo(map)
+        })
         .bindPopup('<a target="_blank" href="/agents/' + agent_id.toLowerCase() + '"/>' + agent_id + '</a>')
+        .addTo(map)
         ;
+
+        agentNameLabels.push(marker.bindTooltip(agent_id, {
+            permanent: true,
+        }));
     }
 };
+
+function resetAgentNameLabelVisibility(){
+    $(".leaflet-tooltip").css("opacity", (
+        map.getZoom() >= 6 ? "1.0" : "0.0")
+    );
+}
+
+map.on("zoomend", resetAgentNameLabelVisibility);
+resetAgentNameLabelVisibility();
 
 //////////////////////////////////////////////////////////////////////////////
 });
